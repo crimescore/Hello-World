066a64bbd747ff11decf288d919ee7133f45626f
PurpleProfessional <97543454+PurpleProfessional@users.noreply.github.com>
2022-02-28 10:23:48 -0500

    Create Hello.cs

---
+++ Hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Hello
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
